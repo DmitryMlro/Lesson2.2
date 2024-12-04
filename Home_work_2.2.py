85c7cc130c8268ae42566fdedceef1fb6d4995eb
Dmitry Miro <131314376+DmitryMlro@users.noreply.github.com>
2024-12-04 15:59:35 +0200

    Update Home_work_2.2.py

--- Home_work_2.2.py
+++ Home_work_2.2.py
@@ -1,3 +1,7 @@
-num = input("Введіть 4-значне число: ")
-revers_num = num[::-1]
-print(revers_num)
+number = int(input("Введіть 5-значне число: "))
+reversed_number = (number % 10) * 10000 + \
+                  ((number // 10) % 10) * 1000 + \
+                  ((number // 100) % 10) * 100 + \
+                  ((number // 1000) % 10) * 10 + \
+                  (number // 10000)
+print(f"Число у зворотньому порядку: {reversed_number}")
